4ddf5587b12ecd0b5266b4cc62d71a3b22925ee9
TheAttentionSeeker5050 <nicolas.castellano.6@hotmail.com>
2022-02-04 16:53:41 -0500

    dver

--- BudgetTracker/BudgetTracker/settings.py
+++ BudgetTracker/BudgetTracker/settings.py
@@ -133,8 +133,3 @@ STATIC_ROOT = BASE_DIR
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import django_heroku
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
